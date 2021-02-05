08855e16a6e896e020e1aaf2978abbee637c21a5
Nathan Pham <nathanpham.me@gmail.com>
2021-02-05 04:41:06 +0000

    shrugs

--- js/config.js
+++ js/config.js
@@ -1,16 +1,18 @@
-export const config = {
-  base: "https://blog.repl.",
+const config = {
+  base: "https://blog.repl.it",
   user: {
     name: "Nathaniel#6988",
     roles: [
       { 
         name: "Hacker",
-        color: "foreground:red"
+        color: "background:red"
       },
       {
         name: "Content Creator",
-        color: "foreground:yellow"
+        color: "background:yellow"
       }
     ]
   }
 }
+
+export default config
--- js/libs/markdown.js
+++ js/libs/markdown.js
@@ -1,11 +1,35 @@
+const table = {
+  "/shrug": "**¯\\_(ツ)_/¯**",
+  "/tableflip":"(╯°□°）╯︵ ┻━┻",
+  "/unflip": "┬─┬ ノ( ゜-゜ノ)" 
+}
+
 const markdown = msg => {
   const bold = /\*\*(.*)\*\*/gim
   const strikethrough = /\~\~(.*)\~\~/gim
   const italics = /\*(.*)\*/gim
-  return msg
-    .replace(bold, '[1m$1[22m')
-    .replace(italics, '[3m$1[23m')
-    .trim()
+
+  let content = msg
+
+  for(const [key, value] of Object.entries(table)) {
+    content = content.replace(new RegExp(key, "gi"), value)
+  }
+
+  content = content
+    .replace(bold, "[1m$1[22m")
+    .replace(italics, "[3m$1[23m")
+
+  let matches = content.match(strikethrough) || []
+  matches.forEach(match => {
+    content = content.replace(match, 
+      match.substring(2, match.length - 2)
+        .split('')
+        .map(char => char + "\u0336")
+        .join('')
+    ) 
+  })
+
+  return content.trim()
 }
 
 export default markdown
