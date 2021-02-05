0f3c800a1c5613bccb342ce276525d435ef3b277
Nathan Pham <nathanpham.me@gmail.com>
2021-02-05 04:27:25 +0000

    markdown

--- js/index.js
+++ js/index.js
@@ -1,7 +1,52 @@
+import markdown from "./libs/markdown.js"
 import colorize from "./libs/color.js"
 import config from "./config.js"
 
+const { base, user } = config
+const roles = generateRoles()
+const input = document.getElementById("input")
 
+const sendMessage = (e) => {
+  if(e.key == "Enter") {
+    let message = input.value
+    let toSend = [ 
+      time(),
+      roles, 
+      user.name + ":", 
+      markdown(message.trim()), 
+      "\n" 
+    ]
+    //  '\r   \n' + time() + '\n\x1b[1m\x1b[90m'+ user +'\x1b[0m: ' + markdown(answer.replace("\\n","\n")) + '\n'
+    if(message.length) {
+      fetch(`${ config.base }/${ encodeURIComponent(
+        `\r   \n\/ \n${ time() }\n${roles} ${user.name}: ${markdown(message)}`
+      ) }`).catch(e => {})
+      input.value = ""
+    }
+  }
+}
+
+function generateRoles() {
+  let array = []
+
+  user.roles.forEach(role => {
+    array.push(
+      colorize(role.color, `[[${ role.name }]]`)
+    )
+  })
+  
+  return array.join(" ")
+}
+
+function time() {
+	let today = new Date(),
+		date = today.getFullYear() + "-" + String(today.getMonth() + 1).padStart(2, '0') + "-" + String(today.getDate()).padStart(2, '0'),
+		time = String(today.getHours()).padStart(2, '0') + ":" + String(today.getMinutes()).padStart(2, '0') + ":" + String(today.getSeconds()).padStart(2, '0')
+
+	return `${date} ${time}\n`
+}
+
+input.addEventListener("keydown", sendMessage)
 /*
 export const config = {
   base: "https://blog.repl.",
--- js/libs/markdown.js
+++ js/libs/markdown.js
@@ -2,10 +2,22 @@ const markdown = msg => {
   const bold = /\*\*(.*)\*\*/gim
   const strikethrough = /\~\~(.*)\~\~/gim
   const italics = /\*(.*)\*/gim
-  return msg
-    .replace(bold, '[1m$1[22m')
-    .replace(italics, '[3m$1[23m')
-    .trim()
+
+  let content = msg
+    .replace(bold, "[1m$1[22m")
+    .replace(italics, "[3m$1[23m")
+
+  let matches = content.match(strikethrough) || []
+  matches.forEach(match => {
+    content = content.replace(match, 
+      match.substring(2, match.length - 2)
+        .split('')
+        .map(char => char + "\u0336")
+        .join('')
+    ) 
+  })
+
+  return content.trim()
 }
 
 export default markdown
